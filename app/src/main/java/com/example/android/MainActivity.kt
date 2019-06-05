c2e5981b1347c337c93a97c43512e18ce5a5d0bd
VEDA <ralphluganob@gmail.com>
2019-06-05 19:50:15 +0800

    Done?

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.android
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // this code goes to source the logo
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+//        rounded.cornerRadius = 30f
+        rounded.isCircular = true
+        appLogo.setImageDrawable(rounded)
     }
+
 }
